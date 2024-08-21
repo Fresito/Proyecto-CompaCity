88a49fbb75c8b042605897d45afe3a9c7e9beba4
Carlos Alfredo MS <al222110720@gmail.com>
2024-08-20 22:58:30 -0600

    Cambios, modificaciones y adicion de dependencias, permiso de almacenamiento y colores de la intefaz

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,15 +54,16 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
 
     // Firebase
-
-    // Desing
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("de.hdodenhof:circleimageview:3.1.0")
-
-    // Import the Firebase BoM
     implementation(platform("com.google.firebase:firebase-bom:33.1.2"))
     implementation("com.google.firebase:firebase-database")
 
+    // Design
+    implementation("androidx.cardview:cardview:1.0.0")
+    implementation("com.google.android.material:material:1.9.0")
+    implementation("de.hdodenhof:circleimageview:3.1.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    // Image Loading
     implementation("com.github.bumptech.glide:glide:4.12.0")
     annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
 
